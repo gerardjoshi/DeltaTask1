cbcf65849990cd403d2bf7c0f6d55cd32ef94b7b
Gerard J <37211017+gerardjoshi@users.noreply.github.com>
2024-06-03 21:42:37 +0530

    Delete alias4a.sh

--- alias4a.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-while read -r line; do
-    read -r v1 v2 <<< $line
-    sudo rm /home/core/mentees/$v1/submittask.sh
-    sudo cp submittask.sh /home/core/mentees/$v1
-    sudo rm /home/core/mentees/$v1/.bashrc
-    sudo echo 'alias submittask="./submittask.sh"'> /home/core/mentees/$v1/.bashrc
-done < menteeDetails.txt
