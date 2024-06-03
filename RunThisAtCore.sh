a284b24cb088c7168f8a38528f8ebd22337b94d1
Gerard J <37211017+gerardjoshi@users.noreply.github.com>
2024-06-03 21:15:46 +0400

    redirecting output of cronjob RunThisAtCore.sh

--- RunThisAtCore.sh
+++ RunThisAtCore.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-(crontab -l; echo "10 10 * 5-7 */3,0 /home/core/crong.sh")|awk '!x[$0]++'|crontab -
+(crontab -l; echo "10 10 * 5-7 */3,0 /home/core/crong.sh > /home/core/cronlogs.txt 2>&1")|awk '!x[$0]++'|crontab -
