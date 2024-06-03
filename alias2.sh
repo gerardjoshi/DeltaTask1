50da806c02fd9ffc3f630bb2c418908730175c97
Gerard J <37211017+gerardjoshi@users.noreply.github.com>
2024-06-03 21:38:11 +0530

    Delete alias2.sh

--- alias2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-while read -r line; do
-    read -r v1 v2 <<< $line
-    sudo rm /home/core/mentees/$v1/domainpref.sh
-    sudo cp domainpref.sh /home/core/mentees/$v1
-    sudo rm /home/core/mentees/$v1/.bashrc
-    sudo echo 'alias domainpref="./domainpref.sh"'> /home/core/mentees/$v1/.bashrc
-    
-done < menteeDetails.txt
